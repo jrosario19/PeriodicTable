5a82d4aea5674a60d6732aed3e46c8c63162e536
jrosario19@hotmail.com <jrosario19@hotmail.com>
2020-02-06 18:20:55 -0400

    Added Zoomin feature

---
+++ PeriodicTable/wwwroot/js/ElementZoomin.js
@@ -0,0 +1,24 @@
+﻿function Zoomin(positionCounter) {
+    var ElementZoomin = $('#ElementTR');
+    ElementZoomin.show();
+    var ElementId = "#E_" + positionCounter;
+    var ElementIdPlain = "E_" + positionCounter;
+    var ElementBackgroundColor = document.getElementById(ElementIdPlain).getAttribute('class');
+
+    
+    $(ElementId).addClass('selectedcell');
+    
+    document.getElementById('ElementTR').setAttribute('class', ElementBackgroundColor);
+    var ElementInfo = $(ElementId).closest('td');
+    var number = ElementInfo.find('div:eq(0)').text();
+    var symbol = ElementInfo.find('div:eq(1)').text();
+    var name = ElementInfo.find('div:eq(2)').text();
+    $('#NumberZoomin').html(number);
+    $('#SymbolZoomin').html(symbol);
+    $('#NameZoomin').html(name);
+}
+function Zoomout(positionCounter) {
+    var ElementId = "#E_" + positionCounter;
+    $(ElementId).removeClass('selectedcell');
+    $('#ElementTR').hide();
+}
